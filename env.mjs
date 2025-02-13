309a5ed088705e024e9cf7590dd5e9899f89646f
torabit <torabit.humble@gmail.com>
2025-02-13 21:08:07 +0900

    feat: configure environment variables with @t3-oss/env-nextjs :gear:

---
+++ env.mjs
@@ -0,0 +1,23 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    /** デプロイかローカルかの変数 */
+    NODE_ENV: z
+      .union([
+        z.literal("development"),
+        z.literal("test"),
+        z.literal("production"),
+      ])
+      .default("development"),
+    /** デプロイの環境変数 */
+    APP_ENV: z
+      .union([z.literal("sandbox"), z.literal("qa"), z.literal("production")])
+      .default("production"),
+  },
+  client: {
+    /** サイトの URL */
+    NEXT_PUBLIC_SITE_URL: z.string().url(),
+  },
+});
